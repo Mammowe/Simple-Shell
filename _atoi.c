4a423108da3970eac16244b63a0efc5cad94b585
Mammowe <kgoputsolerato@yahoo.com>
2023-09-18 14:16:39 -0700

    main

--- _atoi.c
+++ _atoi.c
@@ -8,7 +8,7 @@
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO)&& info->readfd <+ 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -17,29 +17,29 @@ int interactive(info_t *info)
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char , char *delim)
+int is_delim(char, char *delim)
 {
 	while (*delim)
-		if (*delim++== c)
-			return(1);
-	return(0);
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: The characyer to put
+ * @c: The character to put
  * Return: 1 if c is alphabetic, 0 otherwise
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return(1);
+		return (1);
 	else
-		return(0);
+		return (0);
 }
 
 /**
- * _atoi -converts astring to an integer
+ * _atoi - converts astring to an integer
  * @s: the  string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  */
@@ -54,11 +54,11 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <='9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] -'0');
+			result += (s[i] - '0');
 		}
 		else if (flag == 1)
 			flag == 2;
