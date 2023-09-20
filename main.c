5eb019b02134a0d32693d9378e63fb6e24afd7ed
Mammowe <kgoputsolerato@yahoo.com>
2023-09-20 06:28:20 -0700

    built in

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "sheel.h"
+#include "shell.h"
 /**
  * main - entry point
  * @ac: arg count
@@ -13,14 +13,14 @@ int main(int ac, char **av)
 
 		asm ("mov %1, %0\n\t"
 				"add $3, %0"
-				:"=r"(fd)
-				:"r"(fd));
-	if(ac == 2)
+				: "=r"(fd)
+				: "r"(fd));
+	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
-		if(fd == -1)
+		if (fd == -1)
 		{
-			if(errno == EACCES)
+			if (errno == EACCES)
 				exit(126);
 
 			if (errno == ENOENT)
